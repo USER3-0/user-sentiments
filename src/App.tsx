a4cbfcf84628f05acbc5325cd9cda3034261a93d
N-CHO-QFTBN\namu <nameun.cho@user.com.sg>
2023-03-29 15:23:46 +0800

    DELEDTED: unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import Rating from "./components/Rating";
+import SubmissionFeedBack from "./components/SubmissionFeedback";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <Rating max={6} />
+      {/* <Rating max={6} /> */}
+      <SubmissionFeedBack />
     </div>
   );
 }
