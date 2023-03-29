6fbb47b91c5322f54f8dbcf24f0f1a080e526be2
N-CHO-QFTBN\namu <nameun.cho@user.com.sg>
2023-03-29 15:23:26 +0800

    ADDED: submission feedback  component

--- src/components/SubmissionFeedback.tsx
+++ src/components/SubmissionFeedback.tsx
@@ -1,13 +1,32 @@
+import styled from "styled-components";
 import { ReactComponent as SmileICon } from "../assets/icons/smile.svg";
+import BASE_COLORS from "../assets/colors";
 
 type Props = {};
 const SubmissionFeedBack: React.FC<Props> = (props: Props) => {
   return (
-    <div>
+    <Wrapper>
       <SmileICon />
       <p>Thank you! Tell us more.</p>
-    </div>
+    </Wrapper>
   );
 };
 
 export default SubmissionFeedBack;
+
+const Wrapper = styled.div`
+  position: fixed;
+  bottom: 16px;
+  right: 16px;
+  background-color: ${BASE_COLORS.LIGHT_GREY};
+  width: 415px;
+  height: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+
+  & > svg {
+    margin-right: 13.6px;
+  }
+`;
