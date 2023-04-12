c7a46845354d4b085e7f83b84144ce77a71cac58
N-CHO-QFTBN\namu <nameun.cho@user.com.sg>
2023-04-12 17:04:02 +0800

    DELETED: unused comments

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
-
 const root = document.createElement("div");
 root.id = "user-3-0-sentiments-content-view-root";
 document.body.append(root);
