6d4b9da34ef255e3a35eb9ba7a24662b9564cc42
N-CHO-QFTBN\namu <nameun.cho@user.com.sg>
2023-04-12 17:04:28 +0800

    ADDED: UI step logic flow

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,71 @@ import { useState } from "react";
 import Questionnaire from "./components/Questionnaire";
 import Rating from "./components/Rating";
 import SubmissionFeedBack from "./components/SubmissionFeedback";
+import FloatingMenu from "./components/FloatingMenu";
+import { STEPS } from "./assets/constants";
+
+export type StepType = "menu" | "feedback" | "rating" | "questionnaire";
+
+export type UiProps = {
+  isShowing: boolean;
+  onChangeStep: (args: ChangeStepArgs) => void;
+  currentStep: StepType;
+};
+
+type ChangeStepArgs = {
+  stepTo: StepType;
+  stepFrom: StepType;
+  delay?: number;
+};
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [step, setStep] = useState<StepType>("menu");
+  const [isShowingMenu, setIsShowingMenu] = useState<boolean>(true);
+  // UI step: showing menu (0) => rating (1) => feedback => questionnaire (2) => feedback =>  menu (0)
+
+  const handleChangeStep = ({ stepTo, stepFrom, delay }: ChangeStepArgs) => {
+    console.log({ stepFrom, stepTo, delay });
+
+    if (!delay) delay = 500;
+    setTimeout(() => {
+      setStep(stepTo);
+    }, delay);
+
+    if (stepTo === "feedback") {
+      setStep(stepTo);
+      const stepIdx = STEPS.indexOf(stepFrom);
+      const nextStep = STEPS[stepIdx + 1] ?? "menu";
+
+      setTimeout(() => {
+        setStep(nextStep);
+      }, delay);
+    }
+    setIsShowingMenu(false);
+  };
 
   return (
-    <div className="App">
-      {/* <Rating max={6} /> */}
-      {/* <SubmissionFeedBack /> */}
-      <Questionnaire />
+    <div className="">
+      <FloatingMenu
+        isShowing={step === "menu"}
+        onChangeStep={handleChangeStep}
+        currentStep={step}
+      />
+      <Rating
+        max={6}
+        isShowing={step === "rating"}
+        onChangeStep={handleChangeStep}
+        currentStep={step}
+      />
+      <Questionnaire
+        isShowing={step === "questionnaire"}
+        onChangeStep={handleChangeStep}
+        currentStep={step}
+      />
+      <SubmissionFeedBack
+        isShowing={step === "feedback"}
+        onChangeStep={handleChangeStep}
+        currentStep={step}
+      />
     </div>
   );
 }
